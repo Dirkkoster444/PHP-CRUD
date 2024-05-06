68012dc227198860af21bd2db013145618959751
Dirkkoster444 <165992153+Dirkkoster444@users.noreply.github.com>
2024-05-06 12:46:14 +0200

    Add files via upload

---
+++ src/Controller/MainController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\PostType;
+use App\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+class MainController extends AbstractController
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em){
+        $this->em = $em;
+    }
+
+    #[Route('/', name: 'app_main')]
+    public function index(): Response{
+        $posts = $this->em->getRepository(Post::class)->findAll();
+        return $this->render('main/index.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+
+    #[Route('/create-post', name: 'create-post')]
+    public function createPost(Request $request){
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form -> HandleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->persist($post);
+            $this->em->flush();
+
+            $this->addFlash('message', 'Succesvol verstuurd!');
+            return $this->redirectToRoute('app_main');
+        }
+
+        return $this->render('main/post.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    #[Route('edit-post/{id}', name: 'edit-post')]
+    public function editPost(Request $request, $id){
+        $post = $this->em->getRepository(Post::class)->find($id);
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->persist($post);
+            $this->em->flush();
+
+            $this->addFlash('message', 'Succesvol aangepast!');
+            return $this->redirectToRoute('app_main');
+        }
+
+        return $this->render('main/post.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+    #[Route('delete-post/{id}', name: 'delete-post')]
+    public function deletePost(Request $request, $id){
+        $post = $this->em->getRepository(Post::class)->find($id);
+
+            $this->em->remove($post);
+            $this->em->flush();
+
+            $this->addFlash('message', 'Succesvol verwijderd!');
+            return $this->redirectToRoute('app_main');
+        }
+
+
+}
